ad63be2417838d0801fc699f2e07058310a24793
Akshay73c <akshay.chavan20@vit.edu>
2025-02-01 19:26:18 +0530

    add some services

--- backend/src/main/java/com/civic/services/AdminService.java
+++ backend/src/main/java/com/civic/services/AdminService.java
@@ -1,5 +1,14 @@
 package com.civic.services;
 
-public interface AdminService {
+import java.util.List;
 
+import com.civic.pojos.User;
+
+public interface AdminService {
+	User getUserById(long citizenId);
+	List<User> getAllUsers();
+	String updateSelf(User adminDetails);
+	User updateCitizen(long citizenId, User userDetails);
+	String deleteProfile(long adminId);
+	String deleteCitizenById(long citizenId);
 }
